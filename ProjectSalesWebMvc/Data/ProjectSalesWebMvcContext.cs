ca55352fca4892e9e2f39fd436d3a633b6890cd6
Bia <fabianazs@gmail.com>
2019-09-19 15:03:42 -0300

    MySql adaptation and first migration

---
+++ ProjectSalesWebMvc/Data/ProjectSalesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectSalesWebMvc.Models
+{
+    public class ProjectSalesWebMvcContext : DbContext
+    {
+        public ProjectSalesWebMvcContext (DbContextOptions<ProjectSalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectSalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- ProjectSalesWebMvc/Startup.cs
+++ ProjectSalesWebMvc/Startup.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ProjectSalesWebMvc.Models;
 
 namespace ProjectSalesWebMvc
 {
@@ -33,6 +30,10 @@ namespace ProjectSalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjectSalesWebMvcContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("ProjectSalesWebMvcContext"), builder =>
+                    builder.MigrationsAssembly("ProjectSalesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
