8351fe1624b27c7cbd14d6d63229e8d3117884a7
Bia <fabianazs@gmail.com>
2019-09-19 08:33:42 -0300

    Commit inicial

---
+++ ProjectSalesWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectSalesWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
