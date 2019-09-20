c3db8a84bccb7bb0a069434e3007a06100b5f230
Bia <fabianazs@gmail.com>
2019-09-20 11:18:01 -0300

    SellerFormViewModel and Department select component

--- ProjectSalesWebMvc/Controllers/SellersController.cs
+++ ProjectSalesWebMvc/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjectSalesWebMvc.Models;
+using ProjectSalesWebMvc.Models.ViewModels;
 using ProjectSalesWebMvc.Services;
 
 namespace ProjectSalesWebMvc.Controllers
@@ -7,10 +8,12 @@ namespace ProjectSalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -21,8 +24,11 @@ namespace ProjectSalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
---
+++ ProjectSalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProjectSalesWebMvc.Models;
+
+namespace ProjectSalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjectSalesWebMvcContext _context;
+
+        public DepartmentService(ProjectSalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
